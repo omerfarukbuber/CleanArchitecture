21d92fd115752fda5fd70217f9fdb6b04ed25ce4
Ömer Faruk BÜBER <omerfbuber@gmail.com>
2024-11-13 17:13:01 +0300

    Create Product Entity on Domain Layer

---
+++ Domain/Products/Product.cs
@@ -0,0 +1,9 @@
+﻿namespace Domain.Products;
+
+public class Product
+{
+    public long Id { get; set; }
+    public string Name { get; set; } = string.Empty;
+    public decimal Price { get; set; }
+    public List<string> Tags { get; set; } = [];
+}
