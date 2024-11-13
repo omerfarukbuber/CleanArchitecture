4babb370b8cc0dbe3ec78521a75cd7a4badf99d1
Ömer Faruk BÜBER <omerfbuber@gmail.com>
2024-11-13 21:53:02 +0300

    Create DeleteProductCommand and its handler and change Marten options with SoftDeleted turn on in Program.cs

---
+++ Application/Products/DeleteProduct/DeleteProductCommandHandler.cs
@@ -0,0 +1,26 @@
+﻿using Application.Abstractions.Messaging;
+using Domain.Products;
+using Domain.Shared;
+using Marten;
+
+namespace Application.Products.DeleteProduct;
+
+internal sealed class DeleteProductCommandHandler(IDocumentSession session) : ICommandHandler<DeleteProductCommand>
+{
+    private readonly IDocumentSession _session = session;
+
+    public async Task<Result> Handle(DeleteProductCommand request, CancellationToken cancellationToken)
+    {
+        var product = await _session.LoadAsync<Product>(request.Id, cancellationToken);
+        if (product is null)
+        {
+            return Result.Failure(new Error("Product.NotFound",
+                $"The product with the Id = '{request.Id}' couldn't found."));
+        }
+
+        _session.Delete(product);
+        await _session.SaveChangesAsync(cancellationToken);
+
+        return Result.Success();
+    }
+}
