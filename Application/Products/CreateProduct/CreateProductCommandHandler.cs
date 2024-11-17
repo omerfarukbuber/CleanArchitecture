6b4e0095a8c45fb3599b5513e35ddfd4bf9008fe
Ömer Faruk BÜBER <omerfbuber@gmail.com>
2024-11-17 14:55:37 +0300

    CreateProductCommandHandler change for publish an event when new product created

--- Application/Products/CreateProduct/CreateProductCommandHandler.cs
+++ Application/Products/CreateProduct/CreateProductCommandHandler.cs
@@ -1,14 +1,15 @@
 ﻿using Application.Abstractions.Messaging;
+using Application.EventBus;
 using Domain.Products;
 using Domain.Shared.Results;
 using Marten;
 
 namespace Application.Products.CreateProduct;
 
-internal sealed class CreateProductCommandHandler(IDocumentSession session) : ICommandHandler<CreateProductCommand>
+internal sealed class CreateProductCommandHandler(IDocumentSession session, IEventBus eventBus) : ICommandHandler<CreateProductCommand>
 {
     private readonly IDocumentSession _session = session;
-
+    private readonly IEventBus _eventBus = eventBus;
     public async Task<Result> Handle(CreateProductCommand request, CancellationToken cancellationToken)
     {
         var product = new Product
@@ -21,6 +22,13 @@ internal sealed class CreateProductCommandHandler(IDocumentSession session) : IC
         _session.Store(product);
 
         await _session.SaveChangesAsync(cancellationToken);
+
+        await _eventBus.PublishEvent(new ProductCreatedEvent
+        {
+            Id = product.Id,
+            Name = product.Name,
+            Price = product.Price
+        }, cancellationToken);
 
         return Result.Success();
     }
