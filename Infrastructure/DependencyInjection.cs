4a1d5dc8e363799c9bde41bd67ac00ad7f0bc956
Ömer Faruk BÜBER <omerfbuber@gmail.com>
2024-11-17 14:56:17 +0300

    Configure the masstransit and rabbitmq settings and register the services and consumer

--- Infrastructure/DependencyInjection.cs
+++ Infrastructure/DependencyInjection.cs
@@ -1,4 +1,8 @@
-﻿using Microsoft.Extensions.DependencyInjection;
+﻿using Application.EventBus;
+using Application.Products.CreateProduct;
+using Infrastructure.MessageBroker;
+using MassTransit;
+using Microsoft.Extensions.DependencyInjection;
 
 namespace Infrastructure;
 
@@ -6,6 +10,29 @@ public static class DependencyInjection
 {
     public static IServiceCollection AddInfrastructure(this IServiceCollection services)
     {
+        services.AddMassTransit(busConfigurator =>
+        {
+            busConfigurator.SetKebabCaseEndpointNameFormatter();
+
+            busConfigurator.AddConsumer<ProductCreatedEventConsumer>();
+
+            busConfigurator.UsingRabbitMq((context, configurator) =>
+            {
+                var settings = context.GetRequiredService<MessageBrokerSettings>();
+
+                configurator.Host(new Uri(settings.Host), h =>
+                {
+                    h.Username(settings.Username);
+                    h.Password(settings.Password);
+                });
+
+                configurator.ReceiveEndpoint("product-created-event-queue", e =>
+                {
+                    e.ConfigureConsumer<ProductCreatedEventConsumer>(context);
+                });
+            });
+        });
+        services.AddTransient<IEventBus, EventBus>();
         return services;
     }
 }
