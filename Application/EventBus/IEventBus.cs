81dee774446c4862da16531a87c65726c46069fd
Ömer Faruk BÜBER <omerfbuber@gmail.com>
2024-11-17 14:52:49 +0300

    EventBus interface and class for

---
+++ Application/EventBus/IEventBus.cs
@@ -0,0 +1,6 @@
+﻿namespace Application.EventBus;
+
+public interface IEventBus
+{
+    Task PublishEvent<T>(T message, CancellationToken cancellationToken = default) where T : class;
+}
---
+++ Infrastructure/MessageBroker/EventBus.cs
@@ -0,0 +1,14 @@
+﻿using Application.EventBus;
+using MassTransit;
+
+namespace Infrastructure.MessageBroker;
+
+public sealed class EventBus(IPublishEndpoint publishEndpoint) : IEventBus
+{
+    private readonly IPublishEndpoint _publishEndpoint = publishEndpoint;
+
+    public Task PublishEvent<T>(T message,
+        CancellationToken cancellationToken = default) where T : class =>
+        _publishEndpoint.Publish(message,
+            cancellationToken);
+}
