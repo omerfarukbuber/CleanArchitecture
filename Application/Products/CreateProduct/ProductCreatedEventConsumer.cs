763caba8dff478d76a8546c5694fd5e2b2492578
Ömer Faruk BÜBER <omerfbuber@gmail.com>
2024-11-17 14:54:43 +0300

    ProductCreated event and consumer classes

---
+++ Application/Products/CreateProduct/ProductCreatedEventConsumer.cs
@@ -0,0 +1,17 @@
+﻿using MassTransit;
+using Microsoft.Extensions.Logging;
+
+namespace Application.Products.CreateProduct;
+
+public sealed class ProductCreatedEventConsumer(ILogger<ProductCreatedEventConsumer> logger)
+    : IConsumer<ProductCreatedEvent>
+{
+    private readonly ILogger<ProductCreatedEventConsumer> _logger = logger;
+
+    public Task Consume(ConsumeContext<ProductCreatedEvent> context)
+    {
+        _logger.LogInformation("Product created: {@Product}", context.Message);
+        
+        return Task.CompletedTask;
+    }
+}
