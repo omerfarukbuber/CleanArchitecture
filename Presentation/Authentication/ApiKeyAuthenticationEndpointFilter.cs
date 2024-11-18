d6d91b165226ec660ea267b35066d3ef70299b28
Ömer Faruk BÜBER <omerfbuber@gmail.com>
2024-11-18 19:27:25 +0300

    Create and EndpointFilter class for authenticate apikey, and create an example guid apikey on user secrets.json on WebApi project and check the validation of apikey

---
+++ Presentation/Authentication/ApiKeyAuthenticationEndpointFilter.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.Configuration;
+
+namespace Presentation.Authentication;
+
+internal sealed class ApiKeyAuthenticationEndpointFilter(IConfiguration configuration) : IEndpointFilter
+{
+    private const string ApiKeyHeader = "CA-ApiKey";
+    private readonly IConfiguration _configuration = configuration;
+
+    public async ValueTask<object?> InvokeAsync(EndpointFilterInvocationContext context, EndpointFilterDelegate next)
+    {
+        var apiKey = context.HttpContext.Request.Headers[ApiKeyHeader];
+
+        if (!IsApiKeyValid(apiKey))
+        {
+            return Results.Unauthorized();
+        }
+
+        return await next(context);
+    }
+
+    private bool IsApiKeyValid(string? apiKey)
+    {
+        if (string.IsNullOrWhiteSpace(apiKey))
+        {
+            return false;
+        }
+
+        return _configuration.GetValue<string>("ApiKey") == apiKey;
+    } 
+}
